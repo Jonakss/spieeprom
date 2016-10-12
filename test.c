7c395c129cffe62fd6c7ab2eec71ba0e88a4f5da
Jonakss <jona.correapc@gmail.com>
2016-10-12 00:23:21 -0300

    Added test.c and error in read data

--- test.c
+++ test.c
@@ -1,31 +1,34 @@
-#include <iostream>
-
-struct int24{
-    unsigned int data : 24;
-};
-
-void print_binary(int n) {
-    if (n == 0 || n ==1)
-        std::cout << n;
-    else {
-        print_binary(n >> 1);
-        std::cout << (n & 0x1);
-    }
-}
-
-int main(void){
-  long add = 0, capacity = 524287, lastAdd=4, j=0;
-  j = lastAdd - add;
-  std::cout << j << '\n';
-  char data[j];
-
-  for(long i=0; i<=j; i++){
-    data[i] = 'a';
-  }
-
-  for(long i=0; i<=j; i++){
-    std::cout << data[i] << '\n';
-  }
-
-  return 0;
-}
+//#include <iostream>
+//
+//void print_binary(int n) {
+//    if (n == 0 || n ==1)
+//        std::cout << n;
+//    else {
+//        print_binary(n >> 1);
+//        std::cout << (n & 0x1);
+//    }
+//}
+//
+//char* a(){
+//  long add = 0, capacity = 524287, lastAdd=16, j=0;
+//  j = lastAdd - add;
+//  char data[j];
+//
+//  for(long i=0; i<=j; i++){
+//    data[i] = 'a';
+//  }
+//
+//  return data;
+//}
+//
+//int main(void){
+//  char* data = a();
+//
+//  std::cout << sizeof(data) << '\n';
+//
+//  for(long i=0; i<=sizeof(data); i++){
+//    std::cout << data[i] << '\n';
+//  }
+//
+//  return 0;
+//}
